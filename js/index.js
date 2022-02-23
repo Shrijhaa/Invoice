06f89a0ffb8c41a53d84876dc3657e4b10f57feb
Shrijhaa <shrijhaaramesh@gmail.com>
2022-02-23 20:09:39 +0530

    form js fetched values into array

--- js/index.js
+++ js/index.js
@@ -3,6 +3,7 @@ const invoiceForm = document.getElementsByClassName("invoiceform")[0];
 
 // form input
 const fromaddress = document.getElementById("fromaddress");
+
 const fromcity = document.getElementById("fromcity");
 const frompincode = document.getElementById("frompincode");
 const fromcountry = document.getElementById("fromcountry");
@@ -13,13 +14,15 @@ const tocity = document.getElementById("tocity");
 const topincode = document.getElementById("topincode");
 const tocountry = document.getElementById("tocountry");
 const invoicedate = document.getElementById("invoicedate");
-const paymentTerms = document.getElementById("paymentTerms");
+const paymentTerms = document.getElementById("paymentterms");
 const description = document.getElementById("description");
-const itemName = document.getElementById("itemName");
-const quality = document.getElementById("quality");
+const itemName = document.getElementById("itemname");
+const quality = document.getElementById("qty");
 const price = document.getElementById("price");
 const total = document.getElementById("total");
 
+
+var val;
 // application state
 let toggleForm = false;
 let formInput = {
@@ -48,3 +51,103 @@ newInvoiceButton.addEventListener("click", () => {
   toggleForm = !toggleForm;
   formStateChange();
 });
+
+fromaddress.addEventListener("change",() => {
+  val=fromaddress.value;                //store value of input in val
+  formInput["fromaddress"]=val;           //store val in frominput array     
+  
+});
+
+fromcity.addEventListener("change",() => {
+  val=fromcity.value;                
+  formInput["fromcity"]=val;           
+  
+});
+
+frompincode.addEventListener("change",() => {
+  val=frompincode.value;                
+  formInput["frompincode"]=val;           
+  console.log(frompincode);
+});
+
+fromcountry.addEventListener("change",() => {
+  val=fromcountry.value;                
+  formInput["fromcountry"]=val;           
+  
+});
+
+clientname.addEventListener("change",() => {
+  val=clientname.value;                
+  formInput["clientname"]=val;           
+  
+});
+clientmail.addEventListener("change",() => {
+  val=clientmail.value;                
+  formInput["clientmail"]=val;           
+  
+});
+toaddress.addEventListener("change",() => {
+  val=toaddress.value;                
+  formInput["toaddress"]=val;           
+  
+});
+tocity.addEventListener("change",() => {
+  val=tocity.value;                
+  formInput["tocity"]=val;           
+  
+});
+topincode.addEventListener("change",() => {
+  val=topincode.value;                
+  formInput["topincode"]=val;           
+  
+});
+
+tocountry.addEventListener("change",() => {
+  val=tocountry.value;                
+  formInput["tocountry"]=val;           
+  
+});
+
+invoicedate.addEventListener("change",() => {
+  val=invoicedate.value;                
+  formInput["invoicedate"]=val;           
+  
+});
+
+paymentTerms.addEventListener("change",() => {
+  val=paymentTerms.value;                
+  formInput["paymentTerms"]=val;           
+  
+});
+
+description.addEventListener("change",() => {
+  val=description.value;                
+  formInput["description"]=val;           
+  
+});
+
+itemName.addEventListener("change",() => {
+  val=itemName.value;                
+  formInput["itemName"]=val;           
+  
+});
+
+quality.addEventListener("change",() => {
+  val=quality.value;                
+  formInput["quality"]=val;           
+  
+});
+price.addEventListener("change",() => {
+  val=price.value;                
+  formInput["price"]=val;           
+  
+});
+total.addEventListener("change",() => {
+  val=total.value;                
+  formInput["total"]=val;           
+  
+});
+
+if(quality.value != "" && price.value != ""){
+  total.value=quality.value*price.value;
+}
